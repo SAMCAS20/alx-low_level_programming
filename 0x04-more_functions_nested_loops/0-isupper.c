3434e95e5a7df3425a7cf1b49860771fe9dcf632
SAMCAS20 <94729493+SAMCAS20@users.noreply.github.com>
2022-09-21 17:33:56 -0700

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-*function
-*
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
 */
 
 int _isupper(int c)
