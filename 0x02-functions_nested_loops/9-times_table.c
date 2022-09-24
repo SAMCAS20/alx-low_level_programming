24d7d0847490883e370c9a1c2e60e187a7f84f18
SAMCAS20 <94729493+SAMCAS20@users.noreply.github.com>
2022-09-24 08:23:01 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,29 +1,27 @@
 #include "main.h"
+/**
+*times_table - prints the 9 times table
+*
+*Return: returns nothing
+*/
 void times_table(void)
 {
-int rone, cone, d;
-for (rone = 0; rone <= 9; rone++)
+int digit, mult, result;
+for (digit = 0; digit <= 9; digit++)
 {
 _putchar('0');
+
+for (mult = 1; mult <= 9; mult++)
+{
 _putchar(',');
 _putchar(' ');
-for (cone = 1; cone <= 9; cone++)
-{
-d = (rone * cone);
-if ((d / 10) > 0)
-{
-_putchar((d / 10) + '0');
-}
+result = digit * mult;
+if (result <= 9)
+_putchar(' ');
 else
-{
-_putchar(' ');
-}
-_putchar((d % 10) + '0');
-if (cone < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
+_putchar((result / 10) + '0');
+
+_putchar((result % 10) + '0');
 }
 _putchar('\n');
 }
